360bc137ed698747c2f2326b99e9c68613f4c429
Lucas5k <lucascabral112347@gmail.com>
2022-10-28 18:22:56 -0300

    feat: implementação do teste do primeiro req, concluido com sucesso!

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,13 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(1, 'mouse', 'Razer', '28/08/2022', '30/12/2070', 'xp-000015', 'query')
+
+    assert new_product.id == 1
+    assert new_product.nome_do_produto == 'mouse'
+    assert new_product.nome_da_empresa == 'Razer'
+    assert new_product.data_de_fabricacao == '28/08/2022'
+    assert new_product.data_de_validade == '30/12/2070'
+    assert new_product.numero_de_serie == 'xp-000015'
+    assert new_product.instrucoes_de_armazenamento == 'query'
