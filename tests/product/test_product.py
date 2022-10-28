680b03f126fe64b2f35c2458fcde0015637a0ad9
Lucas5k <lucascabral112347@gmail.com>
2022-10-28 20:02:38 -0300

    feat: implementação do teste concluido com sucesso!

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -3,13 +3,13 @@ from inventory_report.inventory.product import Product
 
 def test_cria_produto():
     new_product = Product(
-        1, 'mouse', 'Razer', '28/08/2022', '30/12/2070', 'xp-000015', 'query'
+        1, "mouse", "Razer", "28/08/2022", "30/12/2070", "xp-000015", "query"
     )
 
     assert new_product.id == 1
-    assert new_product.nome_do_produto == 'mouse'
-    assert new_product.nome_da_empresa == 'Razer'
-    assert new_product.data_de_fabricacao == '28/08/2022'
-    assert new_product.data_de_validade == '30/12/2070'
-    assert new_product.numero_de_serie == 'xp-000015'
-    assert new_product.instrucoes_de_armazenamento == 'query'
+    assert new_product.nome_do_produto == "mouse"
+    assert new_product.nome_da_empresa == "Razer"
+    assert new_product.data_de_fabricacao == "28/08/2022"
+    assert new_product.data_de_validade == "30/12/2070"
+    assert new_product.numero_de_serie == "xp-000015"
+    assert new_product.instrucoes_de_armazenamento == "query"
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,11 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1, "mouse", "Razer", "28/08/2022", "30/12/2070", "xp-000015", "query"
+    )
+    to_compare = "O produto mouse fabricado em 28/08/2022 por Razer " \
+        "com validade até 30/12/2070 precisa ser armazenado query."
+
+    assert str(new_product) == to_compare
