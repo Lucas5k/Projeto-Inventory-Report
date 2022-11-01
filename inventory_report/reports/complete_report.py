0381ebc5e59871b13f4ac205a7a7f4ab5e203cc2
Lucas5k <lucascabral112347@gmail.com>
2022-10-31 22:39:53 -0300

    feat: implementação do req 4 concluida com sucesso

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,18 +1,22 @@
 from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
 
 
 class CompleteReport(SimpleReport):
     @staticmethod
     def generate(list_of_report):
         simple_report = SimpleReport.generate(list_of_report)
-        for quantity in list_of_report:
-            one_index_of_list = list_of_report[0].get("nome_da_empresa")
-            if one_index_of_list == quantity.get("nome_da_empresa"):
-                return_names = quantity.get("nome_da_empresa")
-                return_quantitys = len(quantity.get("nome_do_produto"))
+        name = [name["nome_da_empresa"] for name in list_of_report]
+        names = Counter(name)
+        products = ""
+        for name in names:
+            products += (
+                f"- {name}: {names.get(name)}\n"
+            )
+        print(products)
         result = (
             f"{simple_report}\n"
             f"Produtos estocados por empresa:\n"
-            f"- {return_names}: {return_quantitys} "
+            f"{products}"
         )
         return result
